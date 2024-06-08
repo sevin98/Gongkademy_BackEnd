161d6f2e35b9bc3a138ad0103d177ab4a26bae5c
mung96 <mung5079@gmail.com>
2024-06-08 17:08:39 +0900

    refactor: s3 service 부분 코드 변경
    - 변수명 변경: 확장성을 고려해 image->file로 변경
    - 코드 구조 변경: try resource 구문으로 스트림 close코드 삭제

--- src/main/java/com/gongkademy/infra/s3/S3TestController.java
+++ src/main/java/com/gongkademy/infra/s3/S3TestController.java
@@ -1,6 +1,6 @@
 package com.gongkademy.infra.s3;
 
-import com.gongkademy.infra.s3.service.S3ImageService;
+import com.gongkademy.infra.s3.service.S3FileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,16 +10,17 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("api/v1")
 public class S3TestController {
     @Autowired
-    S3ImageService s3ImageService;
+    S3FileService s3ImageService;
     @PostMapping("/s3/upload")
     public ResponseEntity<?> s3Upload(@RequestPart(value = "image", required = false) MultipartFile image){
-        String profileImage = s3ImageService.upload(image);
+        String profileImage = s3ImageService.uploadFile(image);
         return ResponseEntity.ok(profileImage);
     }
 
+
     @DeleteMapping("/s3/delete")
     public ResponseEntity<?> s3delete(@RequestParam String addr){
-        s3ImageService.deleteImageFromS3(addr);
+        s3ImageService.deleteFile(addr);
         return ResponseEntity.ok(null);
     }
 }
--- src/main/java/com/gongkademy/infra/s3/service/ImageService.java
+++
@@ -1,8 +0,0 @@
-package com.gongkademy.infra.s3.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface ImageService {
-    public String upload(MultipartFile image);
-
-}

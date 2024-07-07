db38068814d9bb421ce5b07e68373663d75d5265
sujin <70851874+tnwls000@users.noreply.github.com>
2024-07-08 08:35:35 +0900

    Refactor: SecurityConfig 활성화 관련 수정 (#171)
    * Refactor: Application @Import 어노테이션 삭제
    
    * Refactor: SecurityConfig @EnableWebSecurity 어노테이션 추가

--- src/main/java/com/gongkademy/Application.java
+++ src/main/java/com/gongkademy/Application.java
@@ -6,7 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import({SecurityConfig.class})
 @SpringBootApplication
 public class Application {
 
--- src/main/java/com/gongkademy/global/config/SecurityConfig.java
+++ src/main/java/com/gongkademy/global/config/SecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -33,6 +34,7 @@ import java.util.List;
 @Configuration
 @Log4j2
 @RequiredArgsConstructor
+@EnableWebSecurity
 public class SecurityConfig {
     //passwordEncoder
     //cors설정
